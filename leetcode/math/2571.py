5e1393be1b879004774a135fa0496e045fb3129d
KimJoonSeo <junjun901225@gmail.com>
2023-05-22 23:39:36 +0900

    add a 2571 explaination.

--- leetcode/math/2571.py
+++ leetcode/math/2571.py
@@ -8,6 +8,8 @@ class Solution:
             if log2(n) == int(log2(n)):
                 return res + 1
             x = int(log2(n))
+            # 여기가 포인트!!
+            # n보다 제일 가까운 2개의 2의 거듭 제곱수 사이에서 차이가 적은 수를 n으로 바꾼다.
             n = min(abs(n-2**x), abs(n-2**(x+1)))
             res += 1
         return res
